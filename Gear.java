7e76ab67940df12ee03d092524ddbb8217664ec3
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2022-09-01 10:31:18 +0800

    Add files via upload

---
+++ Gear.java
@@ -0,0 +1,13 @@
+public class Gear {
+    //boots
+    String name;
+    int defense;
+    float weight;
+    String rarity;
+    boolean hasElementalResistance;
+
+    public void description() {
+        System.out.println("This boots is " + name + " with a defense of " + defense + " and weight of " + weight + " and is a " + rarity + " have a resistance " + hasElementalResistance);
+    }
+
+}
