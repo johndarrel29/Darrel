7179ac388d60a5c42f6790ff2fa6b6e1490dcd12
John Darrel Poquiz <111472863+johndarrel29@users.noreply.github.com>
2022-11-07 20:43:21 +0800

    Create Mathpatterns.java

---
+++ Array/multi dimensional array/Mathpatterns.java
@@ -0,0 +1,134 @@
+public class Mathpatterns {
+    public static void main(String[] args) throws Exception {
+  
+        int[][] mdArray = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15 ,16}};
+    
+
+        int sum = 0;
+
+        for (int row = 0; row < mdArray.length; row++) {
+
+            System.out.println(" ");
+
+            for (int column = 0; column < mdArray.length; column++) {
+                System.out.print(mdArray[row][column] + " ");
+            } 
+        }
+  
+        // PATTERN 1
+        System.out.println(" ");
+        System.out.println("1. SUM OF PATTERN 1");
+        
+        for (int row = 0; row < mdArray.length; row++) {
+
+            //System.out.println(" ");
+
+            for (int column = 0; column < mdArray.length; column++) {
+                
+                sum += mdArray[row][column];
+            } 
+        }
+
+        System.out.println("the sum of the array is: " + sum);
+
+        // PATTERN 2
+        System.out.println("");
+        System.out.println("2. SUM OF PATTERN 2");
+
+        sum = 0;
+        for (int row = 0; row < mdArray.length; row++) {
+
+            //System.out.println(" ");
+
+            for (int column = 0; column < mdArray.length; column++) {
+                
+                if (row == 0) {
+                    sum += mdArray[row][column]; 
+                }
+            } 
+        }
+        System.out.println("the sum of this pattern 2 is: " + sum);
+
+        // PATTERN 3
+        System.out.println("");
+        System.out.println("3. SUM OF PATTERN 3");
+
+        sum = 0;
+        for (int row = 0; row < mdArray.length; row++) {
+
+            //System.out.println(" ");
+
+            for (int column = 0; column < mdArray.length; column++) {
+                
+                if (row == 0 || column == 0) {
+                    sum += mdArray[row][column]; 
+                }
+            } 
+        }
+        System.out.println("the sum of this pattern 3 is: " + sum);
+        
+        // PATTERN 4
+        System.out.println("");
+        System.out.println("4. SUM OF PATTERN 4");
+
+        sum = 0;
+        for (int row = 0; row < mdArray.length; row++) {
+
+            //System.out.println(" ");
+
+            for (int column = 0; column < mdArray.length; column++) {
+                
+                if (row == 0 && (column == 0 || column == 3)) {
+                    sum += mdArray[row][column];
+                } else if (row == 3 && (column == 0 || column == 3)) {
+                    sum += mdArray[row][column];
+                }
+            } 
+        }
+        System.out.println("the sum of this pattern 4 is: " + sum);
+        
+        // PATTERN 5
+        System.out.println("");
+        System.out.println("5. SUM OF PATTERN 5");
+
+        sum = 0;
+        for (int row = 0; row < mdArray.length; row++) {
+
+            //System.out.println(" ");
+
+            for (int column = 0; column < mdArray.length; column++) {
+                
+                if (row == 1 && (column == 1 || column == 2)) {
+                    sum += mdArray[row][column];
+                } else if (row == 2 && (column == 1 || column == 2)) {
+                    sum += mdArray[row][column];
+                }
+            } 
+        }
+        System.out.println("the sum of this pattern 5 is: " + sum);
+      
+        // PATTERN 6
+        System.out.println("");
+        System.out.println("6. SUM OF PATTERN 6");
+
+        sum = 0;
+        for (int row = 0; row < mdArray.length; row++) {
+
+            //System.out.println(" ");
+
+            for (int column = 0; column < mdArray.length; column++) {
+                
+                if (row == 1 && (column == 1 || column == 2)) {
+                    sum += mdArray[row][column];
+                } else if (row == 2 && (column == 1 || column == 2)) {
+                    sum += mdArray[row][column];
+                } else if (row == 0 && (column == 0 || column == 3)) {
+                    sum += mdArray[row][column];
+                } else if (row == 3 && (column == 0 || column == 3)) {
+                    sum += mdArray[row][column];
+                }
+            } 
+        }
+        System.out.println("the sum of this pattern 5 is: " + sum);
+    }
+  }
